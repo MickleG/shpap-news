dd8cfc96a84b1900d8418074b7e6b84cc1688b15
Michael Goldberg <micklethenickel@gmail.com>
2020-05-13 17:59:35 -0700

    Got rid of orange theme and made it look spicccccy

--- apiPractice2.js
+++ apiPractice2.js
@@ -1,68 +1,90 @@
 let apiKey = "63ca82ee13794fbe8dc6f2c6b71c0a4d";
-let search = document.getElementById("searchButton");
-search.addEventListener("click", collectSearch);
+let searchQuery = document.getElementById("searchTerm");
 let searchQueryValue;
-let seatchQuery = document.getElementById("searchTerm");
 let searchForm = document.getElementById("searchForm");
-searchForm.style.opacity = 1;
-let counter1 = 0;
-let counter2 = 0;
 
-function collectSearch()
+let homeQuery = document.getElementById("homeButton");
+
+let navButtonQuery = [...document.getElementsByClassName("navSearchQuery")];
+
+let date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth();
+let day = date.getDate();
+
+if(month == 0)
 {
-	let date = new Date();
-	let year = date.getFullYear();
-	let month = date.getMonth() + 1;
-	let day = date.getDate();
+	month = 12;
+	year--;
+}
 
-	if(month < 10)
-	{
-		month = "0"+month;
-	}
-
-	
-
-	searchQueryValue = document.getElementById("searchTerm").value;
-	fetch("https://newsapi.org/v2/everything?language=en&q="+searchQueryValue+"&from="+year+"-"+month+"-"+day+"&sortBy=publishedAt&apiKey="+apiKey)
-	.then(data => data.json())
-	.then(response => 
+if(month < 10)
 {
-	let articles = response.articles;
+	month = "0" + month;
+}
 
-	let wrapper = document.getElementById("wrapper");
-	wrapper.innerHTML = "";
-	articles.forEach(article => 
-	{
-		
-		let title = document.createElement("h2");
-		title.innerHTML = article.title;
-		wrapper.appendChild(title);
+// homeQuery.addEventListener("click", showHome());
 
-		let content = document.createElement("p");
-		content.innerHTML = article.content;
-		wrapper.appendChild(content);
-
-		let image = document.createElement("img");
-		image.src = article.urlToImage;
-		image.classList.add("articleImage");
-		content.appendChild(image);
-
-		let line = document.createElement("hr");
-		line.style.color = "#e28700";
-		wrapper.appendChild(line);
-
-	})
-
-	if(counter2 == 0)
-	{
-	searchForm.style.position = "absolute";
-	searchForm.style.animation = "moveRight 1s ease 1";
-	setTimeout(function() 
+navButtonQuery.forEach(category => 
+{
+	let navTopic = category.innerHTML;
+	category.addEventListener("click", function()
 		{
-			searchForm.style.animation = "keepStill 1s ease infinite";
-		}
-		, 1000);
-		counter2++;
-	}
+			searchNews(navTopic);
+		});
 })
+
+searchQuery.addEventListener("keydown", function(keyId)
+{
+	if(keyId.code == "Enter")
+	{
+		searchQueryValue = document.getElementById("searchTerm").value;
+		searchNews(searchQueryValue);
+	}
+});
+
+
+function searchNews(topic)
+{
+	fetch("https://newsapi.org/v2/everything?language=en&q="+topic+"&from="+year+"-"+month+"-"+day+"&sortBy=publishedAt&apiKey="+apiKey)
+		.then(data => data.json())
+		.then(response => 
+		{
+			let articles = response.articles;
+			console.log(response);
+			let wrapper = document.getElementById("wrapper");
+			wrapper.innerHTML = "";
+			let zValue = 0;
+			articles.forEach(article => 
+			{
+				if (article.length > 0) 
+				{
+  				article.first().css({ 'z-index' : zValue });
+ 				}
+ 				
+				let articleArea = document.createElement("article");
+				articleArea.classList.add("articleArea");
+
+				let title = document.createElement("h2");
+				title.classList.add("articleTitle");
+				title.innerHTML = article.title;
+				articleArea.appendChild(title);
+
+				let content = document.createElement("p");
+				content.classList.add("articleContent");
+				content.innerHTML = article.content;
+				articleArea.appendChild(content);
+
+				if(article.urlToImage != null)
+				{
+				let image = document.createElement("img");
+				image.src = article.urlToImage;
+				image.classList.add("articleImage");
+				articleArea.appendChild(image);
+				}
+				wrapper.appendChild(articleArea);
+				zValue++;
+			})
+
+		})
 }
